802059f3309135aaac5a224af835fb2d13a4ad13
fred belotte <fredbelotte@outlook.com>
2020-06-24 15:08:23 -0500

    update controller routing template [#173239135]

--- aspnet/RVTR.Lodging.WebApi/Controllers/LodgingController.cs
+++ aspnet/RVTR.Lodging.WebApi/Controllers/LodgingController.cs
@@ -13,7 +13,7 @@ namespace RVTR.Lodging.WebApi.Controllers
   [ApiController]
   [ApiVersion("0.0")]
   [EnableCors("public")]
-  [Route("api/v{version:apiVersion}/[controller]")]
+  [Route("{version:apiVersion}/[controller]")]
   public class LodgingController : ControllerBase
   {
     private readonly ILogger<LodgingController> _logger;
