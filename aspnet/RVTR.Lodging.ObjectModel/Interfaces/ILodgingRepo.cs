927de155815aacf00cd146df2d64598a1fbe9b20
kirtipatel98 <kirti.bapa@gmail.com>
2020-08-13 13:54:33 -0700

    add Lodging repository and interfaces for repository classes

---
+++ aspnet/RVTR.Lodging.ObjectModel/Interfaces/ILodgingRepo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RVTR.Lodging.ObjectModel.Models;
+
+namespace RVTR.Lodging.ObjectModel.Interfaces
+{
+  public interface ILodgingRepo
+  {
+    Task<IEnumerable<LodgingModel>> AvailableLodgings();
+  }
+}
