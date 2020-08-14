7960facc30200dbafc43d113b696ef26895eb8b6
Josh Bertrand <joshuabertrand@protonmail.com>
2020-08-14 14:44:12 -0400

    Modify UnitOfWork to use LodgingRepo instead of the generic Repository

--- aspnet/RVTR.Lodging.DataContext/Repositories/UnitOfWork.cs
+++ aspnet/RVTR.Lodging.DataContext/Repositories/UnitOfWork.cs
@@ -3,30 +3,30 @@ using RVTR.Lodging.ObjectModel.Models;
 
 namespace RVTR.Lodging.DataContext.Repositories
 {
-  /// <summary>
-  /// Represents the _UnitOfWork_ repository
-  /// </summary>
-  public class UnitOfWork
-  {
-    private readonly LodgingContext _context;
-
-    public virtual Repository<LodgingModel> Lodging { get; }
-    public virtual Repository<RentalModel> Rental { get; set; }
-    public virtual Repository<ReviewModel> Review { get; set; }
-
-    public UnitOfWork(LodgingContext context)
-    {
-      _context = context;
-
-      Lodging = new Repository<LodgingModel>(context);
-      Rental = new Repository<RentalModel>(context);
-      Review = new Repository<ReviewModel>(context);
-    }
-
     /// <summary>
-    /// Represents the _UnitOfWork_ `Commit` method
+    /// Represents the _UnitOfWork_ repository
     /// </summary>
-    /// <returns></returns>
-    public async Task<int> CommitAsync() => await _context.SaveChangesAsync();
-  }
+    public class UnitOfWork
+    {
+        private readonly LodgingContext _context;
+
+        public virtual LodgingRepo Lodging { get; }
+        public virtual Repository<RentalModel> Rental { get; set; }
+        public virtual Repository<ReviewModel> Review { get; set; }
+
+        public UnitOfWork(LodgingContext context)
+        {
+            _context = context;
+
+            Lodging = new LodgingRepo(context);
+            Rental = new Repository<RentalModel>(context);
+            Review = new Repository<ReviewModel>(context);
+        }
+
+        /// <summary>
+        /// Represents the _UnitOfWork_ `Commit` method
+        /// </summary>
+        /// <returns></returns>
+        public async Task<int> CommitAsync() => await _context.SaveChangesAsync();
+    }
 }
