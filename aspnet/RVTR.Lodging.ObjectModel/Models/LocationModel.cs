ffecd8d86409efd1038dc5bb0ade1eed477744eb
Josh Bertrand <joshuabertrand@protonmail.com>
2020-08-18 16:04:41 -0400

    Change LocationModel to include LodgingId property

--- aspnet/RVTR.Lodging.ObjectModel/Models/LocationModel.cs
+++ aspnet/RVTR.Lodging.ObjectModel/Models/LocationModel.cs
@@ -3,24 +3,28 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RVTR.Lodging.ObjectModel.Models
 {
-  /// <summary>
-  /// Represents the _Location_ model
-  /// </summary>
-  public class LocationModel : IValidatableObject
-  {
-    public int Id { get; set; }
-
-    public AddressModel Address { get; set; }
-
-    public string Latitude { get; set; }
-
-    public string Longitude { get; set; }
-
     /// <summary>
-    /// Represents the _Location_ `Validate` method
+    /// Represents the _Location_ model
     /// </summary>
-    /// <param name="validationContext"></param>
-    /// <returns></returns>
-    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) => new List<ValidationResult>();
-  }
+    public class LocationModel : IValidatableObject
+    {
+        public int Id { get; set; }
+
+        public int LodgingId { get; set; }
+
+        public LodgingModel lodging { get; set; }
+
+        public AddressModel Address { get; set; }
+
+        public string Latitude { get; set; }
+
+        public string Longitude { get; set; }
+
+        /// <summary>
+        /// Represents the _Location_ `Validate` method
+        /// </summary>
+        /// <param name="validationContext"></param>
+        /// <returns></returns>
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) => new List<ValidationResult>();
+    }
 }
