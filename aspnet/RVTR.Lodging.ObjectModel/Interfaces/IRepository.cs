c9a70a932890c3202b38a6a17ab33fee57b8f63b
Noah Funtanilla <noahfuntanilla6@gmail.com>
2020-08-18 09:44:41 -0600

    Added documentation to IRepo

--- aspnet/RVTR.Lodging.ObjectModel/Interfaces/IRepository.cs
+++ aspnet/RVTR.Lodging.ObjectModel/Interfaces/IRepository.cs
@@ -4,6 +4,10 @@ using System.Threading.Tasks;
 
 namespace RVTR.Lodging.ObjectModel.Interfaces
 {
+  /// <summary>
+  /// Interface that provides CRUD methods for different entities depending on the given generic type
+  /// </summary>
+  /// <typeparam name="TEntity">The Entity which needs CRUD operations</typeparam>
   public interface IRepository<TEntity> where TEntity : class
   {
     Task DeleteAsync(int id);
