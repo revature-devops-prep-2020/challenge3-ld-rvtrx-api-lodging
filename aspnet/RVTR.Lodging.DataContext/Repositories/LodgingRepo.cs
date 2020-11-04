d872f5a211a810e834c1a17255375452ee953af1
David Dowd <ddowd97@gmail.com>
2020-11-04 10:14:12 -0600

    Removed Unnecessary Namespace

--- aspnet/RVTR.Lodging.DataContext/Repositories/LodgingRepo.cs
+++ aspnet/RVTR.Lodging.DataContext/Repositories/LodgingRepo.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using RVTR.Lodging.ObjectModel.Interfaces;
